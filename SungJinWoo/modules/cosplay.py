81a57044958bccb23647393ca87155a9b92276cd
SOME1HING <yashprakash2005@gmail.com>
2023-06-06 18:31:26 +0530

    Updated API for cosplay images.
    Update to Superior Sugoi-API for 2000 high quality cosplay images.

--- SungJinWoo/modules/cosplay.py
+++ SungJinWoo/modules/cosplay.py
@@ -6,12 +6,12 @@ from SungJinWoo import telethn as meow
 
 @meow.on(events.NewMessage(pattern="^/cosplay"))
 async def waifu(event):
-  r = requests.get("https://waifu-api.vercel.app").json() #api credit- @YASH_SHARMA_1807 on telegram
+  r = requests.get("https://sugoi-api.vercel.app/cosplay").json()['url']
   await event.reply(file=r)
   
 @meow.on(events.NewMessage(pattern="^/lewd"))
 async def waifu(event):
-  r = requests.get("https://waifu-api.vercel.app/items/1").json()
+  r = requests.get("https://sugoi-api.vercel.app/ncosplay").json()['url']
   await event.reply(file=r)
 
 __mod_name__ = "Cosplay"
